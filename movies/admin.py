2308ec99dcecffd7db9d2d3635870f53f28a09b9
keny Exantus <keny.exantus@gmail.com>
2022-06-28 21:40:22 -0400

    django project

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
+#register your models here
+"""now that we have our data base set up the next thing is to allow the staff in at our imaginary video rental store
+to populate the list of movies to do this we need to create an administration panel for them, howver we do not have to do this manually 
+since django already comes with a powerful administration interface using the /admin in our django brower will direct us to an administration login site
+which comes from the auth app in the settungs.py file of our package,
 
-# Register your models here.
+
+next we need to create a superuser to create a login to access the admin website, to do so we need to use the "python manage.py createsuperuser" commmand in terminal 
+this will prompt you to create a usrname, an email address and a password, after that you will be able to access the website using the password created, there we can manage the 
+list of users and groups
+ """
+from django.contrib import admin
+from  .models import Genre, Movie # to be able to manage our genre and movies we need to import the Genre and Movie classes from the models module in the current folder(denoted by a "." in beginning)
+class GenreAdmin(admin.ModelAdmin): #here we are created a class to be able to customize our model 
+    list_display = ('id','name') # we are overriding the list_display attribute to display what we want to show
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id','number_in_stock', 'daily_rate')
+    exclude = ('date_created',)
+
+# these lines are telling django  to register the models in the administration interface
+admin.site.register(Genre,GenreAdmin) # now we need to pass the second argument as the class we created for the changes to register on the website
+admin.site.register(Movie,MovieAdmin)
+
+
+"""1. now we need to customize the admin to make it more user friendly go to the models.py module under the Classes for more notes 
+
+ 2. next we need display the list of our movies in the public area of the website """
+
+
--- vidly/urls.py
+++ vidly/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include # we also import the include function to be able to recieve the url config we made in the movies app
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # every django app has an administrative panel which is a path that maps anything that starts with admit to admin.site.urls
+    path('movies/', include('movies.urls')),
+    
+    
+#we than add a new path object, that tells our vidly app that any path that starts with movies(first arg) should be handed off to the url configaration in the movies app(second arg) 
+#we dont add "movies.py" inside the inlcude function because django automatically append the extension and load the module,
+#whenever we send a request that starts with "movies" django will chop off the prefex(movies/) and send the remaining string to the "movies.urls" module, which is why we use an empty string to represent the root of the app
+
 ]
+
+"""now on the terminal we can run the server using "python" manage.py runserver,
+initially when we run our server we will get a page not found error but if we use /movies and enter we will see our message "Hello World" 
+which means we have sucessfully mapped a url endpoint to a view function"""
